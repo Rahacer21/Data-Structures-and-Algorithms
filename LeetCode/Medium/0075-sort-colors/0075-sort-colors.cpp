1b38541529ecdc951a2819800876196c593f9d80
Rahacer21 <cserahularora@gmail.com>
2026-01-30 19:39:05 +0530

    0075-sort-colors .cpp 01-30-2026 Medium 0 ms (100%) 11.7 MB (11.17%)

---
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;     //for 0 
+        int j=0;     //for 1
+        int k=n-1;   //for 2
+        while(j<=k){
+            if(nums[j]==2){
+                swap(nums[j],nums[k]);
+                k--;
+            }
+            else if(nums[j]==1){
+                j++;
+            }
+            else{
+                swap(nums[j],nums[i]);
+                i++;
+                j++;
+            }
+        }
+    }
+};
