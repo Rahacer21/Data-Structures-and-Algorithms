d60d6dbc231ceb008644932ea25ce3537a9ef273
Rahacer21 <cserahularora@gmail.com>
2026-03-03 16:42:33 +0530

    1545-find-kth-bit-in-nth-binary-string .cpp 03-03-2026 Medium 0 ms (100%) 7.9 MB (63.07%)

--- LeetCode/Medium/1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ LeetCode/Medium/1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    string rev(string s){
-        reverse(s.begin(),s.end());
-        return s;
-    }
-    string invert(string s){
-        string str="";
-        for(char ch:s){
-            if(ch=='0') str+="1";
-            else str+="0"; 
-        }
-        return str;
-    }
     char findKthBit(int n, int k) {
-        vector<string> s;
-        s.push_back("0");
-        for(int i=1;i<n;i++){
-            s.push_back(s[i-1]+"1"+rev(invert(s[i-1])));
+        int len=pow(2,n)-1;
+        int mid=len/2;
+        if(n==1) return '0';
+        if(k==mid+1) return '1';
+        else if(k<=mid){
+            return findKthBit(n-1,k);
         }
-
-        return s[n-1][k-1];
+        else{
+            char ch=findKthBit(n-1,len-(k-1));
+            return ch=='0'?'1':'0';
+        }
     }
 };
